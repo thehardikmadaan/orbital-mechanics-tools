5d8a48e10a78c01f5a6eccefc4f6d6e7c290577c
thehardikmadaan <hardik.madaan5@gmail.com>
2026-04-16 12:33:26 +0530

    Implement detailed orbit plotting in `draw_orbits` with LEO, GEO, and Earth visuals

--- visualization/plot_orbit.py
+++ visualization/plot_orbit.py
@@ -25,3 +25,33 @@ class OrbitPlotter(FigureCanvasQTAgg):
 
     def draw_orbits(self, alt1_km, alt2_km):
         """Clears the canvas and redraws the orbits based on user input."""
+        self.ax.clear() # clear prev drawing
+
+        r_earth = 6371
+        r1 = r_earth + alt1_km
+        r2 = r_earth + alt2_km
+
+        # array of angles from o to 2pi
+        theta = np.linspace(0, 2*np.pi, 100)
+        # Earth coordinates
+        earth_x = r_earth* np.cos(theta)
+        earth_y = r_earth* np.sin(theta)
+
+        #LEO coordinates (r1)
+        leo_x = r1* np.cos(theta)
+        leo_y = r1* np.sin(theta)
+
+        #GEO coordinates (r2)
+        geo_x = r2* np.cos(theta)
+        geo_y = r2* np.sin(theta)
+
+        # plotting lines
+        self.ax.fill(earth_x, earth_y, color='#00d4ff', alpha=0.3, label="Earth")
+        self.ax.plot(leo_x, leo_y, color='#7b61ff', linestyle='--', label="Initial Orbit")
+        self.ax.plot(geo_x, geo_y, color='#ff6b35', linestyle='--', label="Target Orbit")
+
+        # Formatting
+        self.ax.set_aspect('equal')  # Prevents circular orbits from looking like ovals
+        self.ax.legend(loc='upper right', facecolor='#020408', edgecolor='none', labelcolor='#e8edf5')
+        self.fig.tight_layout()
+        self.draw()
